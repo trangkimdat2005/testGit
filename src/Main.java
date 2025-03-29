fccf081e2825130d5d72e4ae96f4b92e74f84188
trangkimdat2005 <trangkimdatst2005@gmail.com>
2025-03-29 21:06:43 +0700

    new

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setTitle("Đăng nhập");
+        primaryStage.getIcons().add(new Image("anhTieuDe.jpg"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("dangNhap.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("DangNhap");
+        primaryStage.show();
+    }
+}
